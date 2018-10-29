cf2a336f40ed22ecc53525a9896581f1d63e98d7
avest <avest@cs-vml-17.dyn.cs.sfu.ca>
2018-10-28 20:05:31 -0700

    Removing debugging information

--- extract_optical_flow.py
+++ extract_optical_flow.py
@@ -15,34 +15,8 @@ for split in ["train", "test", "val"]:
             flow = cv2.calcOpticalFlowFarneback(img1, img2, flow=None, pyr_scale=.5,
                 levels=3, winsize=13, iterations=3, poly_n=5, poly_sigma=1.1,
                 flags=cv2.OPTFLOW_FARNEBACK_GAUSSIAN)
-
-            if np.isnan(flow).any():
-                raise ValueError("nan in flow")
-
-            if np.isinf(flow).any():
-                raise ValueError("inf in flow")
-
             mag, ang = cv2.cartToPolar(flow[:, :,0], flow[:, :,1])
             mag = np.clip(mag, 0, np.inf)
-
-            if np.isnan(ang).any():
-                raise ValueError("nan in ang")
-            if np.isinf(ang).any():
-                raise ValueError("inf in ang")
-
-            if np.isnan(mag).any():
-                raise ValueError("nan in mag before norm")
-            if np.isinf(mag).any():
-                print(np.max(mag))
-                print(np.min(mag))
-                raise ValueError("inf in mag before norm")
-
-            mag2 = cv2.normalize(mag, dst=None,alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
-
-            if np.isnan(mag2).any():
-                raise ValueError("nan in mag after norm")
-            if np.isinf(mag2).any():
-                raise ValueError("inf in mag after norm")
-
+            mag = cv2.normalize(mag, dst=None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
             flow = np.stack((mag, ang))
             np.save("./data/optical_flow/%s_frames/frame_%d" % (split, i), flow)
